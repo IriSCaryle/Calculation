43988c8be34eeaaf68aa31838379dfe14f769418
saikora- <c20027@std.it-college.ac.jp>
2021-05-23 21:20:39 +0900

    桁が消えないやつ修正＆オブジェクト張り替え→スプライト張り替えに仕様変更

--- Assets/Scripts/NumberToSprite.cs
+++ Assets/Scripts/NumberToSprite.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class NumberToSprite : MonoBehaviour
 {
@@ -9,18 +8,37 @@ public class NumberToSprite : MonoBehaviour
 
     [SerializeField] int startitme;
     int nowtime;
-    [SerializeField] GameObject[] object_time = new GameObject[10];//画像の数字xはUnity上のElement xの数字に合わせてね
+    int beforetime;
+    [SerializeField] GameObject time_box;//これの中のSpriteRendererに画像をつける
+    [SerializeField] Sprite[] sprite_time = new Sprite[10];//画像の数字xはUnity上のElement xの数字に合わせてね
 
     GameObject[] time_image = new GameObject [10];//桁数に合わせて画像を保存するとこ
+    SpriteRenderer[] time_spriteR = new SpriteRenderer[10];
 
     public int Digit(int num)
     {
         return (num == 0) ? 1 : ((int)Mathf.Log10(num) + 1);//nowtimeの桁数を取得
     }
 
+    public int BTDight(int num)
+    {
+        return (num == 0) ? 1 : ((int)Mathf.Log10(num) + 1);//beforetimeの桁数を取得
+    }
+
     void Start()
     {
         nowtime = startitme;
+
+        for(int i =0;i<Digit (nowtime); i++)
+        {
+            time_image[i] = Instantiate(
+                time_box,
+                new Vector3(i * -1, 0, 0),//位置は適当なので調整お願い
+                Quaternion.identity
+                );
+
+            time_spriteR[i] = time_image[i].GetComponent<SpriteRenderer>();
+        }
         InstSplite();
     }
 
@@ -32,9 +50,11 @@ public class NumberToSprite : MonoBehaviour
 
     void CountCange()
     {
+        beforetime = nowtime;
         nowtime--;
+        if (BTDight(beforetime) != Digit(nowtime)) Destroy(time_image[BTDight(beforetime) - 1]);//使い終えた桁を削除
+
         InstSplite();
-        timecount = 0;
     }
 
     void InstSplite()
@@ -43,15 +63,10 @@ public class NumberToSprite : MonoBehaviour
 
         for (int i = 0; i < Digit(nowtime); i++)
         {
-            Destroy(time_image[i]);//指定桁を破壊
-
             int num = (int)(nowtime / Mathf.Pow(10, i)) % 10;//iの位の数値を取得
 
-            time_image[i] = Instantiate(
-                object_time[num],
-                new Vector3 (i* -1,0,0),//位置は適当なので調整お願い
-                Quaternion.identity
-                );
+            time_spriteR[i].sprite = sprite_time[num];
         }
+        timecount = 0;
     }
 }
